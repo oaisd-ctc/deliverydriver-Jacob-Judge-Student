b6406826c2acc7862cdd2da14a4e0231ac65258d
Jacob-Judge-Student <jajuf70@student.oaisd.org>
2023-03-08 09:43:01 -0500

    i finishd the game
    hello i complted the game

--- Assets/Driver Project.cs
+++ Assets/Driver Project.cs
@@ -5,19 +5,29 @@ using UnityEngine;
 public class DriverProject : MonoBehaviour
 {
     [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float moveSpeed = 0.01f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float slowSpeed = 18f;
+    [SerializeField] float boostSpeed = 32f;
+   
+   
     void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = slowSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
     }
 }
